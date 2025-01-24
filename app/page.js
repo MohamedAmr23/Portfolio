45808aa6695f44d6ed9fdaf2ca5f265b04b07c4c
MohamedAmr23 <amrm96754@gmail.com>
2025-01-24 04:06:03 +0200

    navbar

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import { Fragment } from "react";
+import Navbar from "./components/Navbar.jsx";
 
 export default function Home() {
   return (
     <Fragment>
-      
+      <Navbar/>
     </Fragment>
   );
 }

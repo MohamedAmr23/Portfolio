dfc21f2b31082d34ead69771dfb228d9fd3149ea
MohamedAmr23 <amrm96754@gmail.com>
2025-02-17 01:22:59 +0200

    set logic of dark mode

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,7 +2,7 @@
 import Image from "next/image.js";
 import React, { useEffect, useState } from "react";
 import { assets } from "../../assets/assets.js";
-const Navbar = () => {
+const Navbar = ({isDarkMode,setIsDarkMode}) => {
   const [open,setOpen] = useState(false)
   const [isScroll , setIsScroll] = useState(false)
   useEffect(()=>{
@@ -16,10 +16,10 @@ const Navbar = () => {
   },[])
   return (
     <>
-    <div className="fixed top-0 right-0 w-11/12 translate-y-[-80%] -z-10">
+    <div className="fixed top-0 right-0 w-11/12 translate-y-[-80%] -z-10 dark:hidden">
         <Image src={assets.header_bg_color} alt="" className="w-full"/>
     </div>
-      <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex justify-between items-center z-50 ${isScroll?'bg-white bg-opacity-50 backdrop-blur-lg shadow-sm':''}`}>
+      <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex justify-between items-center z-50 ${isScroll?'bg-white bg-opacity-50 backdrop-blur-lg shadow-sm dark:bg-darkTheme dark:shadow-white/20':''}`}>
         <a href="#top">
           {" "}
           <Image
@@ -56,7 +56,7 @@ const Navbar = () => {
           </li>
         </ul>
         <div className="flex items-center gap-4">
-          <button><Image src={assets.moon_icon} alt="" className="w-6"/></button>
+          <button onClick={()=>setIsDarkMode(prev=>!prev)}><Image src={isDarkMode?assets.sun_icon:assets.moon_icon} alt="" className="w-6"/></button>
           <a
             href="#contact"
             className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo"
